ec8b6ccb41f916416065bdb6f03ac9ed652c59df
Ben Wegher <bwegs14@gmail.com>
2014-06-03 21:39:35 -0400

    Added user input validation to email and text alerts

--- AndroidAppA/src/myApp/androidappa/AddNewEmailAlert.java
+++ AndroidAppA/src/myApp/androidappa/AddNewEmailAlert.java
@@ -9,42 +9,79 @@ import android.widget.RadioButton;
 import android.widget.Toast;
 
 public class AddNewEmailAlert extends Activity {
-	EditText editTextMessage;
+	EditText alertName;
+	EditText emailAdd;
+	// location
+	EditText message;
 	RadioButton arriveRadio;
 	RadioButton leaveRadio;
 	
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.add_new_email_alert);
-        editTextMessage=(EditText)findViewById(R.id.editText1);
+        alertName = (EditText)findViewById(R.id.editText1);
+        emailAdd = (EditText)findViewById(R.id.editText2);
+        message = (EditText)findViewById(R.id.editText4);
         arriveRadio = (RadioButton)findViewById(R.id.radio0);
         leaveRadio = (RadioButton)findViewById(R.id.radio1);
-
     }
     
-	public void addItem(View V)
+	public void addAlert(View V)
 	{
-		// get the Entered  message
-		String item = editTextMessage.getText().toString();
+		
+		// get user data and convert to Strings
+		String name = alertName.getText().toString();
+		String email = emailAdd.getText().toString();
+		String text = message.getText().toString();
+		
+		// **reminder** also need to check for duplicate names
+		if(checkEmpty(name)) { 
+			Toast.makeText(AddNewEmailAlert.this,
+					   "Give your alert a name!",
+					   Toast.LENGTH_LONG).show();
+			return;
+		} else if (checkEmpty(email)) {
+			Toast.makeText(AddNewEmailAlert.this,
+					   "You forgot to enter an email address",
+					   Toast.LENGTH_LONG).show();
+			return;
+		} else if (!email.contains("@") || !email.contains(".")) {
+			Toast.makeText(AddNewEmailAlert.this,
+					   "That email address isn't valid",
+					   Toast.LENGTH_LONG).show();
+			return;
+		} else if (checkEmpty(text)) {
+			Toast.makeText(AddNewEmailAlert.this,
+					   "You need to enter a message to send to " + email,
+					   Toast.LENGTH_LONG).show();
+			return;
+		}
+		
 		Intent intentMessage = new Intent();
 		
 		// Debugging toast
 		Toast.makeText(AddNewEmailAlert.this,
-					   "Added new text alert: " + item,
+					   "Added new email alert: " + name + " " + email + " " + text,
 					   Toast.LENGTH_LONG).show();
 		
 		
-		if(arriveRadio.isChecked()) {
-			intentMessage.putExtra("WHEN", "ENTER");
-		} else {
-			intentMessage.putExtra("WHEN", "LEAVE");
-		} 
+//		if(arriveRadio.isChecked()) {
+//			intentMessage.putExtra("WHEN", "ENTER");
+//		} else {
+//			intentMessage.putExtra("WHEN", "LEAVE");
+//		} 
 		
 		// put the message in Intent
-		intentMessage.putExtra("ITEM", item);
+		//intentMessage.putExtra("ITEM", item);
 
-		setResult(2,intentMessage);
+		//setResult(2,intentMessage);
 	
-        finish();
+        //finish();
+	}
+	
+	private boolean checkEmpty(String s) {
+		if(s.matches(""))
+			return true;
+		return false;
 	}
 }
--- AndroidAppA/src/myApp/androidappa/AddNewTextAlert.java
+++ AndroidAppA/src/myApp/androidappa/AddNewTextAlert.java
@@ -9,42 +9,95 @@ import android.widget.RadioButton;
 import android.widget.Toast;
 
 public class AddNewTextAlert extends Activity {
-	EditText editTextMessage;
+	EditText alertName;
+	EditText phoneAdd;
+	// location
+	EditText message;
 	RadioButton arriveRadio;
 	RadioButton leaveRadio;
 	
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.add_new_text_alert);
-        editTextMessage=(EditText)findViewById(R.id.editText1);
+        alertName = (EditText)findViewById(R.id.editText1);
+        phoneAdd = (EditText)findViewById(R.id.editText2);
+        message = (EditText)findViewById(R.id.editText4);
         arriveRadio = (RadioButton)findViewById(R.id.radio0);
         leaveRadio = (RadioButton)findViewById(R.id.radio1);
-
     }
     
-	public void addItem(View V)
+	public void addAlert(View V)
 	{
-		// get the Entered  message
-		String item = editTextMessage.getText().toString();
-		Intent intentMessage = new Intent();
+		// get user data and convert to Strings
+		String name = alertName.getText().toString();
+		String phone = phoneAdd.getText().toString();
+		String text = message.getText().toString();
 		
-		// Debugging toast
-		Toast.makeText(AddNewTextAlert.this,
-					   "Added new text alert: " + item,
-					   Toast.LENGTH_LONG).show();
-		
-		
-		if(arriveRadio.isChecked()) {
-			intentMessage.putExtra("WHEN", "ENTER");
-		} else {
-			intentMessage.putExtra("WHEN", "LEAVE");
-		} 
-		
-		// put the message in Intent
-		intentMessage.putExtra("ITEM", item);
+		// **reminder** also need to check for duplicate names
+				if(checkEmpty(name)) { 
+					Toast.makeText(AddNewTextAlert.this,
+							   "Give your alert a name!",
+							   Toast.LENGTH_LONG).show();
+					return;
+				} else if (checkEmpty(phone)) {
+					Toast.makeText(AddNewTextAlert.this,
+							   "You forgot to enter a phone number",
+							   Toast.LENGTH_LONG).show();
+					return;
+				} else if (phone.length() != 7 && phone.length() != 10) {
+					Toast.makeText(AddNewTextAlert.this,
+							   "That phone number isn't valid",
+							   Toast.LENGTH_LONG).show();
+					return;
+				} else if (!onlyNumeric(phone)) {
+					Toast.makeText(AddNewTextAlert.this,
+							   "Phone number must contain only numbers " + phone,
+							   Toast.LENGTH_LONG).show();
+					return;
+				} else if (checkEmpty(text)) {
+					Toast.makeText(AddNewTextAlert.this,
+							   "You need to enter a message to send to " + phone,
+							   Toast.LENGTH_LONG).show();
+					return;
+				}
+				
+				Intent intentMessage = new Intent();
+				
+				// Debugging toast
+				Toast.makeText(AddNewTextAlert.this,
+							   "Added new text alert: " + name + " " + phone + " " + text,
+							   Toast.LENGTH_LONG).show();
+				
+				
+//				if(arriveRadio.isChecked()) {
+//					intentMessage.putExtra("WHEN", "ENTER");
+//				} else {
+//					intentMessage.putExtra("WHEN", "LEAVE");
+//				} 
+				
+				// put the message in Intent
+				//intentMessage.putExtra("ITEM", item);
 
-		setResult(2,intentMessage);
-	
-        finish();
+				//setResult(2,intentMessage);
+			
+		        //finish();
 	}
+	
+	private boolean checkEmpty(String s) {
+		if(s.matches(""))
+			return true;
+		return false;
+	}
+	
+	private boolean onlyNumeric(String s) {
+		try {
+			Long.parseLong(s);
+		} catch (NumberFormatException e) {
+			return false;
+		}
+		
+		return true;
+		
+	}
+	
 }
