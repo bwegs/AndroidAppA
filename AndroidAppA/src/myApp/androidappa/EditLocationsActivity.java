c31b51b90ead75758e003c82477b49d529549d6c
Ben Wegher <bwegs14@gmail.com>
2014-06-21 22:09:56 -0400

    Added a google map fragment to the locations activity -- modified it to zoom to the user's current location

--- AndroidAppA/src/myApp/androidappa/EditLocationsActivity.java
+++ AndroidAppA/src/myApp/androidappa/EditLocationsActivity.java
@@ -1,13 +1,88 @@
 package myApp.androidappa;
 
 import android.app.Activity;
+import android.app.Dialog;
+import android.content.Context;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.LatLng;
 
 public class EditLocationsActivity extends Activity {
-	public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.edit_locations);
 
-    }
+	GoogleMap googleMap;
+	Location lastKnownLocation;
+
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.edit_locations);
+
+		// Getting Google Play availability status
+		int status = GooglePlayServicesUtil
+				.isGooglePlayServicesAvailable(getBaseContext());
+
+		// Showing status
+		if (status != ConnectionResult.SUCCESS) { // Google Play Services are
+													// not available
+
+			int requestCode = 10;
+			Dialog dialog = GooglePlayServicesUtil.getErrorDialog(status, this,
+					requestCode);
+			dialog.show();
+
+		} else { // Google Play Services are available
+
+			// Getting reference to the SupportMapFragment of activity_main.xml
+			MapFragment fm = (MapFragment) getFragmentManager()
+					.findFragmentById(R.id.map);
+
+			// Getting GoogleMap object from the fragment
+			googleMap = fm.getMap();
+
+			// Enabling MyLocation Layer of Google Map
+			googleMap.setMyLocationEnabled(true);
+
+			// Getting LocationManager object from System Service
+			// LOCATION_SERVICE
+			LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+
+			// Creating a criteria object to retrieve provider
+			Criteria criteria = new Criteria();
+
+			// Getting the name of the best provider
+			String provider = locationManager.getBestProvider(criteria, true);
+
+			// Getting Current Location
+			Location location = locationManager.getLastKnownLocation(provider);
+
+			if (location != null) {
+				// center the map on user
+				centerMapOnMyLocation();
+			}
+		}
+	}
+
+	private void centerMapOnMyLocation() {
+		LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+		Criteria criteria = new Criteria();
+
+		Location location = locationManager
+				.getLastKnownLocation(locationManager.getBestProvider(criteria,
+						false));
+
+		if (location != null) {
+			googleMap
+					.animateCamera(CameraUpdateFactory.newLatLngZoom(
+							new LatLng(location.getLatitude(), location
+									.getLongitude()), 15));
+		}
+	}
+
 }
